cc8af7ea9e35164b4537da00ad7f1ab45843c009
leejianzhi <lijianzhimomo@gmail.com>
2018-10-27 22:55:06 -0400

    Login Page Finished
    To Do:
    
    1. Improve validation methods
    2. Backend function for Forgot password

--- static-html/js/userInterface.js
+++ static-html/js/userInterface.js
@@ -1,55 +1,40 @@
-(function () {
-    'use strict';
+Vue.use(VeeValidate);
 
-    angular
-        .module('app')
-        .controller('LoginController', LoginControllerm,'RegisterController', RegisterController);
+const newrule = {
+  getMessage(field, params, data) {
+      return (data && data.message) || 'Something went wrong';
+  },
+  validate(value) {
+    return new Promise(resolve => {
+      resolve({
+        valid: username.includes(value.toLowerCase()) ? false : !! value,
+      });
+    });
+  }
+};
+  VeeValidate.Validator.extend('checkuser',newrule);
+    const LoginForm = new Vue({
+        el: '#login-form',
+        data: {
+            errors: [],
+            email: null,
+            password: null
+        },
+      created: function(){
+       
+      },
+        methods: {
+            processForm: function() {
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, AuthenticationService, FlashService) {
-        var vm = this;
-
-        vm.login = login;
-
-        (function initController() {
-            // reset login status
-            AuthenticationService.ClearCredentials();
-        })();
-
-        function login() {
-            vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('/');
-                } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
-                }
-            });
-        };
-    }
-
-
-    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
-        var vm = this;
-
-        vm.register = register;
-
-        function register() {
-            vm.dataLoading = true;
-            UserService.Create(vm.user)
-                .then(function (response) {
-                    if (response.success) {
-                        FlashService.Success('Registration successful', true);
-                        $location.path('/login');
-                    } else {
-                        FlashService.Error(response.message);
-                        vm.dataLoading = false;
+                this.$validator.validateAll().then((result) => {
+                    if (result) {
+                       
+                       alert('Form validated succesfully');
                     }
                 });
-        }
-    }
 
-})();
+
+             }
+        }
+
+    });
