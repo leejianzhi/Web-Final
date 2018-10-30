066064ef04045738305981092f6820da584b8ba2
leejianzhi <lijianzhimomo@gmail.com>
2018-10-29 21:36:07 -0400

    Register almost done
    To Do:
    1. Add tags can selete personal interest
    2. Page can edit user's profile

--- static-html/js/register.js
+++ static-html/js/register.js
@@ -1,46 +1,26 @@
 Vue.use(VeeValidate);
- var username = [
-    'admin',
-    'password',
-    'administartor'
-]
-const newrule = {
-  getMessage(field, params, data) {
-      return (data && data.message) || 'Something went wrong';
-  },
-  validate(value) {
-    return new Promise(resolve => {
-      resolve({
-        valid: username.includes(value.toLowerCase()) ? false : !! value,
-        data: { message: `${value} has already been taken` }
-      });
-    });
-  }
-};
-  VeeValidate.Validator.extend('checkuser',newrule);
-    const signupForm = new Vue({
-        el: '#signup-form',
-        data: {
-            name: '',
-            email: '',
-            username: '',
-            password: ''
-        },
-      created: function(){
-       
-      },
-        methods: {
-            processForm: function() {
 
-                this.$validator.validateAll().then((result) => {
-                    if (result) {
-                       
-                       alert('Form validated succesfully');
-                    }
-                });
-
-
-            }
+export default {
+    name: 'app',
+    data () {
+        return {
+            user: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: ''
+            },
+            submitted: false
         }
-
-    });
+    },
+    methods: {
+        handleSubmit(e) {
+            this.submitted = true;
+            this.$validator.validate().then(valid => {
+                if (valid) {
+                    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.user))
+                }
+            });
+        }
+    }
+};
